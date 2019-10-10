c8994223860f349ef491af98ddcd6a49b0ed352a
Lance Jernigan <Lance@LanceJernigan.com>
2019-10-10 15:52:59 -0400

    Add icon support to social menu (#23)
    * Add meta retrieval functionality
    
    * Retrieve social menu specific meta
    
    * Update view to handle new icon key

--- wp-content/plugins/_core/includes/helpers/menu.php
+++ wp-content/plugins/_core/includes/helpers/menu.php
@@ -8,28 +8,51 @@ use function _core\helpers\utils\merge;
 use function Functional\map;
 use function Functional\select_keys;
 
-function simplify_menu_items( $menu_items ) {
+function simplify_menu_items( $menu_items, $menu_item_fields ) {
 	return map(
 		$menu_items,
-		function( $item ) {
+		function( $item ) use ($menu_item_fields) {
+			$meta = get_menu_meta( $item, $menu_item_fields );
+
 			return merge(
 				select_keys( (array) $item, [ 'children', 'class', 'url', 'id', 'target' ] ),
 				[
-					'children' => simplify_menu_items( $item->children ),
+					'children' => simplify_menu_items( $item->children, $menu_item_fields ),
 					'title'    => $item->title,
-				]
+				],
+				$meta
 			);
 		}
 	);
 }
 
-function get( $menu_name ) {
+function get( $menu_name, $menu_fields = [], $menu_item_fields = [] ) {
 	$menu = new Timber\Menu( $menu_name );
+	$meta  = get_menu_meta($menu, $menu_fields);
 
 	return merge(
 		select_keys( (array) $menu, [ 'items', 'name', 'slug', 'id' ] ),
 		[
-			'items' => simplify_menu_items( $menu->items ),
-		]
+			'items' => simplify_menu_items( $menu->items, $menu_item_fields ),
+		],
+		$meta
+	);
+}
+
+function get_menu_meta($menu, $fields) {
+	return array_reduce(
+		$fields,
+		function ($_meta, $field) use ($menu) {
+			$field_name = is_array($field)
+				? $field[0]
+				: $field;
+			$field_key = is_array($field)
+				? $field[1]
+				: $field;
+			$field_value = get_field($field_name, $menu);
+			$_meta[$field_key] = $field_value;
+			return $_meta;
+		},
+		[]
 	);
 }
