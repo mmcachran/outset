8dc1eb55a43c798c13b11bd05129a6a082545de3
Mizner <mike@mizner.io>
2019-10-10 21:06:14 -0400

    formatting from linting

--- wp-content/plugins/_core/includes/helpers/menu.php
+++ wp-content/plugins/_core/includes/helpers/menu.php
@@ -11,7 +11,7 @@ use function Functional\select_keys;
 function simplify_menu_items( $menu_items, $menu_item_fields ) {
 	return map(
 		$menu_items,
-		function( $item ) use ($menu_item_fields) {
+		function( $item ) use ( $menu_item_fields ) {
 			$meta = get_menu_meta( $item, $menu_item_fields );
 
 			return merge(
@@ -28,7 +28,7 @@ function simplify_menu_items( $menu_items, $menu_item_fields ) {
 
 function get( $menu_name, $menu_fields = [], $menu_item_fields = [] ) {
 	$menu = new Timber\Menu( $menu_name );
-	$meta  = get_menu_meta($menu, $menu_fields);
+	$meta = get_menu_meta( $menu, $menu_fields );
 
 	return merge(
 		select_keys( (array) $menu, [ 'items', 'name', 'slug', 'id' ] ),
@@ -39,18 +39,18 @@ function get( $menu_name, $menu_fields = [], $menu_item_fields = [] ) {
 	);
 }
 
-function get_menu_meta($menu, $fields) {
+function get_menu_meta( $menu, $fields ) {
 	return array_reduce(
 		$fields,
-		function ($_meta, $field) use ($menu) {
-			$field_name = is_array($field)
+		function ( $_meta, $field ) use ( $menu ) {
+			$field_name          = is_array( $field )
 				? $field[0]
 				: $field;
-			$field_key = is_array($field)
+			$field_key           = is_array( $field )
 				? $field[1]
 				: $field;
-			$field_value = get_field($field_name, $menu);
-			$_meta[$field_key] = $field_value;
+			$field_value         = get_field( $field_name, $menu );
+			$_meta[ $field_key ] = $field_value;
 			return $_meta;
 		},
 		[]
