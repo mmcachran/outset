6856ab95c6ab7643d967d5408195d481fdedf77d
Lance Jernigan <lance@lancejernigan.com>
2019-11-04 09:15:31 -0500

    Remove php cs

--- tools/tasks/php.js
+++ tools/tasks/php.js
@@ -2,13 +2,14 @@ const { spawn } = require('child_process');
 import { paths } from '../utils/paths';
 
 function php(cb) {
-  return spawn(`${paths.root}/vendor/squizlabs/php_codesniffer/bin/phpcs`, [], { stdio: 'inherit' })
-    .on('close', cb)
-    .on('exit', () => cb())
-    .on('error', err => {
-      console.error(err);
-      process.exit(err);
-    });
+  return cb();
+  // return spawn(`${paths.root}/vendor/squizlabs/php_codesniffer/bin/phpcs`, [], { stdio: 'inherit' })
+  //   .on('close', cb)
+  //   .on('exit', () => cb())
+  //   .on('error', err => {
+  //     console.error(err);
+  //     process.exit(err);
+  //   });
 }
 
 export { php };
