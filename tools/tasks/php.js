db2ae5ea92f549804102fe0aad0ea8d5a8b54d85
Jim McCaslin <jmccaslin@pyxl.com>
2020-01-27 14:48:05 -0600

    Temporarily removed php from gulp

--- tools/tasks/php.js
+++ tools/tasks/php.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 function php(cb) {
-  return spawn('./vendor/squizlabs/php_codesniffer/bin/phpcs', [], { stdio: 'inherit' })
+  return spawn('phpcs', [], { stdio: 'inherit' })
     .on('close', cb)
     .on('exit', () => cb())
     .on('error', err => {
