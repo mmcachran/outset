fe02e7ebec97ce95f5821ab74908c16b6ec7fe88
Jim McCaslin <jmccaslin@pyxl.com>
2020-01-27 14:20:20 -0600

    Updated tools to fix phpcs

--- tools/tasks/php.js
+++ tools/tasks/php.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 function php(cb) {
-  return spawn('phpcs', [], { stdio: 'inherit' })
+  return spawn('./vendor/squizlabs/php_codesniffer/bin/phpcs', [], { stdio: 'inherit' })
     .on('close', cb)
     .on('exit', () => cb())
     .on('error', err => {
