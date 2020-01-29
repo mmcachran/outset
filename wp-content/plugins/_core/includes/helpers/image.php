7d8d7a8a766571e6f7188609bcd494745cb584cb
Jim McCaslin <jmccaslin@pyxl.com>
2020-01-29 17:20:15 -0600

    Updated image helper with correct sizing.

--- wp-content/plugins/_core/includes/helpers/image.php
+++ wp-content/plugins/_core/includes/helpers/image.php
@@ -2,23 +2,31 @@
 
 namespace _core\helpers\image;
 
+use Timber;
 use function _view\utils\merge;
 use function Functional\map;
 
 function create_sizes_urls( $src, $sizes ) {
-	return array_reverse(
-		map(
-			$sizes,
-			function( $size ) use ( $src ) {
-				return merge(
-					$size,
-					[
-						'url' => dirname( $src ) . "/{$size['file']}",
-					]
-				);
-			}
-		)
+	$urls = map(
+		$sizes,
+		function( $size ) use ( $src ) {
+			return merge(
+				$size,
+				[
+					'url' => dirname( $src ) . "/{$size['file']}",
+				]
+			);
+		}
 	);
+
+	usort(
+		$urls,
+		function( $a, $b ) {
+			return $a['width'] <=> $b['width'];
+		}
+	);
+
+	return array_reverse( $urls );
 }
 
 function reformat_from_timber( $timber_image ) {
@@ -26,11 +34,28 @@ function reformat_from_timber( $timber_image ) {
 		return [];
 	};
 
+	$img_src = wp_get_attachment_image_src( $timber_image->ID, 'full' );
+
+	$sizes   = create_sizes_urls( $timber_image->src, $timber_image->sizes );
+	$sizes[] = [
+		'file'      => basename( $timber_image->src ),
+		'url'       => $timber_image->src,
+		'height'    => ( is_array( $img_src ) ) ? $img_src[2] : '',
+		'width'     => ( is_array( $img_src ) ) ? $img_src[1] : '',
+		'mime-type' => $timber_image->post_mime_type,
+	];
+
 	return [
 		'alt'       => $timber_image->alt,
 		'src'       => $timber_image->src,
+		'height'    => ( is_array( $img_src ) ) ? $img_src[2] : '',
+		'width'     => ( is_array( $img_src ) ) ? $img_src[1] : '',
 		'srcset'    => $timber_image->srcset,
 		'img_sizes' => $timber_image->img_sizes,
-		'sizes'     => create_sizes_urls( $timber_image->src, $timber_image->sizes ),
+		'sizes'     => $sizes,
 	];
+}
+
+function image_from_post_id( $post ) {
+	return reformat_from_timber( new Timber\Image( get_post_thumbnail_id( $post ) ) );
 }
