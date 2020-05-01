e59fca84b463b273e24591e0d6a1e987b96b4b40
Mizner <mike@mizner.io>
2020-05-01 07:09:47 -0500

    swap to has_key check

--- wp-content/plugins/_core/includes/filters/blocks/hero.php
+++ wp-content/plugins/_core/includes/filters/blocks/hero.php
@@ -4,14 +4,15 @@ namespace _core\filters\blocks;
 
 use Timber;
 use _core\helpers\image;
+
+use function _core\helpers\utils\has_key;
 use function Functional\map;
 
 function hero( $data ) {
 	return map(
 		$data,
 		function( $value, $key ) use ( $data ) {
-
-			if ( in_array( $key, [ 'image' ] ) ) {
+			if ( has_key( $key, [ 'image' ] ) ) {
 				return image\reformat_from_timber( new Timber\Image( $value ) );
 			}
 
