7f0e4c55950fc274f2889ff2ca2a3251aff29135
Mizner <mike@mizner.io>
2020-05-01 11:44:31 -0500

    working accordion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 import lazyLoading from './features/lazyLoading';
 import mobileMenu from './features/mobileMenu';
+import accordion from './features/accordion';
 
+accordion();
 lazyLoading();
 mobileMenu();
