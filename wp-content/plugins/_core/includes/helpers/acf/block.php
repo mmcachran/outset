e0a27cbb7f6de9c00cd9c93ac5efb1f7a42f8b41
Mizner <mike@mizner.io>
2020-05-01 06:34:30 -0500

    add doc blocks and clean up filter callback

--- wp-content/plugins/_core/includes/helpers/acf/block.php
+++ wp-content/plugins/_core/includes/helpers/acf/block.php
@@ -12,24 +12,84 @@ use function _core\helpers\utils\underscores_to_dashes;
 use function Functional\select_keys;
 use function _view\utils\env_check;
 
-function create( $args = [] ) {
-	if ( ! has_every_key( [ 'label', 'slug', 'fields' ], $args ) ) {
-		return;
-	}
-
-	if ( ! function_exists( 'acf_register_block_type' ) ) {
-		return;
-	}
-
-	acf_register_block_type( create_block_type_args( $args ) );
-
-	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
-		return;
-	}
-
-	acf_add_local_field_group( create_register_group_args( $args ) );
+/**
+ * Create arguments for block field registration in ACF.
+ *
+ * @param [type] $args
+ * @return void
+ */
+function create_register_group_args( $args ) {
+	return [
+		'key'      => "block/{$args['slug']}",
+		// phpcs:disable WordPress.WP.I18n.NoEmptyStrings
+		// translators: %s: Title of field group
+		'title'    => sprintf( __( '%s', 'core' ), $args['label'] ),
+		'fields'   => field_shorthand_translator( $args['slug'], $args['fields'] ),
+		'location' => [
+			[
+				[
+					'param'    => 'block',
+					'operator' => '==',
+					'value'    => 'acf/block-' . underscores_to_dashes( $args['slug'] ),
+				],
+			],
+		],
+	];
 }
 
+/**
+ * Handler to feed data to our rendered template.
+ *
+ * @param [type] $block
+ * @param string $content
+ * @param boolean $is_preview
+ * @return void
+ */
+function filter_data_handler( $block, $content = '', $is_preview = false ) {
+	return merge(
+		get_fields(),
+		select_keys( $block, [ 'align', 'mode', 'title' ] ),
+		[
+			// 'base' is inherited from template.php
+			'is_preview' => $is_preview,
+			'classes'    => setup_classes(
+				[
+					'custom-block',
+					has_key( 'align', $block ) ? "align{$block['align']}" : null,
+					has_key( 'className', $block ) ? $block['className'] : null,
+				]
+			),
+		]
+	);
+}
+
+/**
+ * Render template callback handler with filters.
+ *
+ * @param [type] $block
+ * @param string $content
+ * @param boolean $is_preview
+ * @return void
+ */
+function render_callback_handler( $block, $content = '', $is_preview = false ) {
+	render(
+		"block/{$block['slug']}",
+		apply_filters(
+			'_core/block/global_data',
+			apply_filters(
+				"_view/block/{$block['slug']}/data",
+				filter_data_handler( $block, $content, $is_preview )
+			)
+		)
+	);
+}
+
+/**
+ * Register Block for WP Block Editor.
+ *
+ * @param [type] $args
+ * @return void
+ */
 function create_block_type_args( $args ) {
 	return merge(
 		[
@@ -54,48 +114,26 @@ function create_block_type_args( $args ) {
 	);
 }
 
-function create_register_group_args( $args ) {
-	return [
-		'key'      => "block/{$args['slug']}",
-		// phpcs:disable WordPress.WP.I18n.NoEmptyStrings
-		// translators: %s: Title of field group
-		'title'    => sprintf( __( '%s', 'core' ), $args['label'] ),
-		'fields'   => field_shorthand_translator( $args['slug'], $args['fields'] ),
-		'location' => [
-			[
-				[
-					'param'    => 'block',
-					'operator' => '==',
-					'value'    => 'acf/block-' . underscores_to_dashes( $args['slug'] ),
-				],
-			],
-		],
-	];
-}
+/**
+ * Block Creation / Registration entry function.
+ *
+ * @param array $args
+ * @return void
+ */
+function create( $args = [] ) {
+	if ( ! has_every_key( [ 'label', 'slug', 'fields' ], $args ) ) {
+		return;
+	}
 
-function render_callback_handler( $block, $content = '', $is_preview = false ) {
-	render(
-		"block/{$block['slug']}",
-		apply_filters(
-			'_core/block/global_data',
-			apply_filters(
-				sprintf( '_view/block/%s/data', $block['slug'] ),
-				merge(
-					get_fields(),
-					select_keys( $block, [ 'align', 'mode', 'title' ] ),
-					[
-						// 'base' is inherited from template.php
-						'is_preview' => $is_preview,
-						'classes'    => setup_classes(
-							[
-								'custom-block',
-								has_key( 'align', $block ) ? "align{$block['align']}" : null,
-								has_key( 'className', $block ) ? $block['className'] : null,
-							]
-						),
-					]
-				)
-			)
-		)
-	);
+	if ( ! function_exists( 'acf_register_block_type' ) ) {
+		return;
+	}
+
+	acf_register_block_type( create_block_type_args( $args ) );
+
+	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+		return;
+	}
+
+	acf_add_local_field_group( create_register_group_args( $args ) );
 }
