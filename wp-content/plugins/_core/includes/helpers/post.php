498ffeeec099b7926b54585c670bb58f41536961
Mizner <mike@mizner.io>
2020-05-05 15:36:16 -0500

    add post type option to post query and return excerpt as string

--- wp-content/plugins/_core/includes/helpers/post.php
+++ wp-content/plugins/_core/includes/helpers/post.php
@@ -4,13 +4,37 @@ namespace _core\helpers\post;
 
 use _core\helpers\image;
 use _core\helpers\taxonomy;
+
+use function _core\helpers\utils\has_key;
 use function Functional\map;
 
+/**
+ * Simplify returned post from a single timber query
+ *
+ * @param array $post
+ * @return array
+ */
 function simplify( $post = [] ) {
+
+	if ( 'testimonial' === $post->post_type ) {
+		return [
+			'title'          => $post->title(),
+			'content'        => $post->content(),
+			'excerpt'        => (string) $post->preview()->length( 25 )->read_more( 'read more' ),
+			'author'         => $post->author->display_name,
+			'link'           => get_the_permalink( $post->id ),
+			'post_type'      => $post->post_type,
+			'location'       => $post->meta( 'location' ),
+			'position'       => $post->meta( 'position' ),
+			'name'           => $post->meta( 'name' ),
+			'featured_image' => image\reformat_from_timber( $post->thumbnail() ),
+		];
+	}
+
 	return [
 		'title'          => $post->title(),
 		'content'        => $post->content(),
-		'excerpt'        => $post->preview()->length( 25 )->read_more( 'read more' ),
+		'excerpt'        => (string) $post->preview()->length( 25 )->read_more( 'read more' ),
 		'author'         => $post->author->display_name,
 		'date'           => [
 			'posted'   => $post->date( 'M d, Y' ),
